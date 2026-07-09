72a220f6808c55979e9152cc588acf7ba6b259f6
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:06:29 +0530

    Create 3939-ProcessStringWithSpecialOperationsIi

---
+++ 3939-ProcessStringWithSpecialOperationsIi/3939-ProcessStringWithSpecialOperationsIi.java
@@ -0,0 +1,47 @@
+// Last updated: 7/9/2026, 3:00:50 PM
+class Solution {
+    public char processStr(String s, long k) {
+        int n = s.length();
+
+        long[] len = new long[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                len[i + 1] = len[i] + 1;
+            } else if (c == '*') {
+                len[i + 1] = Math.max(0, len[i] - 1);
+            } else if (c == '#') {
+                len[i + 1] = len[i] * 2;
+            } else { // '%'
+                len[i + 1] = len[i];
+            }
+        }
+
+        if (k >= len[n]) {
+            return '.';
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                if (k == len[i]) {
+                    return c;
+                }
+            } else if (c == '*') {
+                // nothing to do
+            } else if (c == '#') {
+                long half = len[i];
+                if (k >= half) {
+                    k -= half;
+                }
+            } else { // '%'
+                k = len[i] - 1 - k;
+            }
+        }
+
+        return '.';
+    }
+}
