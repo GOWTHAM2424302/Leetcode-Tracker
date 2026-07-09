06878ace584b7814cc4dd501cfec3fd567b676a5
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:06:43 +0530

    Create 2099-NumberOfStringsThatAppearAsSubstringsInWord

---
+++ 2099-NumberOfStringsThatAppearAsSubstringsInWord/2099-NumberOfStringsThatAppearAsSubstringsInWord.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:01:04 PM
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+
+        for (String pattern : patterns) {
+            if (word.contains(pattern)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
