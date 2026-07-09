c90e92d152f44dde2d034dc96d81b0301cf23477
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:07:28 +0530

    Create 658-FindKClosestElements

---
+++ 658-FindKClosestElements/658-FindKClosestElements.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 3:01:49 PM
+import java.util.*;
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        int left = 0;
+        int right = arr.length - k;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = left; i < left + k; i++) {
+            ans.add(arr[i]);
+        }
+
+        return ans;
+    }
+}
