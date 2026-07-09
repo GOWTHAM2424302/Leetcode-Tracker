7a61c709268a01401f2c809bac84eb606206b38f
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:08:53 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 3:03:14 PM
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+
+        int first = 1;
+        int second = 2;
+
+        for(int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+}
