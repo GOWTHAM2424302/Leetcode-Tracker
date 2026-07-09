2640edef7ea85d993238a006f84c09a83624959d
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:08:02 +0530

    Create 207-CourseSchedule

---
+++ 207-CourseSchedule/207-CourseSchedule.java
@@ -0,0 +1,37 @@
+// Last updated: 7/9/2026, 3:02:23 PM
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        List<Integer>[] graph = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+            graph[i] = new ArrayList<>();
+
+        int[] indegree = new int[numCourses];
+
+        for (int[] p : prerequisites) {
+            graph[p[1]].add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                q.offer(i);
+
+        int count = 0;
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            count++;
+
+            for (int next : graph[cur]) {
+                if (--indegree[next] == 0)
+                    q.offer(next);
+            }
+        }
+
+        return count == numCourses;
+    }
+}
