4a490efc1348826c8f80c847c7d20dd5e3ac9b0c
GOWTHAM2424302 <25adl02@drngpit.ac.in>
2026-07-09 15:07:53 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:02:14 PM
+class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        int[] factors = {2, 3, 5};
+
+        for (int f : factors) {
+            while (n % f == 0) {
+                n /= f;
+            }
+        }
+
+        return n == 1;
+    }
+}
