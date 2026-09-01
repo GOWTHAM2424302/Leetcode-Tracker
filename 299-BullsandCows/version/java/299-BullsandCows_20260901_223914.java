68e65bfb95a2f007b21b6306af1977d47b2640c5
GOWTHAM N P <25adl02@drngpit.ac.in>
2026-09-01 22:39:17 +0530

    Create 299-BullsandCows

---
+++ 299-BullsandCows/version/java/299-BullsandCows_20260901_223914.java
@@ -0,0 +1,31 @@
+// Last updated: 9/1/2026, 10:39:14 PM
+1class Solution {
+2    public String getHint(String secret, String guess) {
+3        int bulls = 0;
+4        int cows = 0;
+5
+6        int[] count = new int[10];
+7
+8        for (int i = 0; i < secret.length(); i++) {
+9            char s = secret.charAt(i);
+10            char g = guess.charAt(i);
+11
+12            if (s == g) {
+13                bulls++;
+14            } else {
+15                if (count[s - '0'] < 0) {
+16                    cows++;
+17                }
+18
+19                if (count[g - '0'] > 0) {
+20                    cows++;
+21                }
+22
+23                count[s - '0']++;
+24                count[g - '0']--;
+25            }
+26        }
+27
+28        return bulls + "A" + cows + "B";
+29    }
+30}
